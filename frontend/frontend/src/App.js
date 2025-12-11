a3ac4b2f2ef6067b1ee3c562973df5615cc53dc6
Anuroop P S <anuroopps2001@gmail.com>
2025-12-11 11:13:19 +0000

    changed app text from feature branch

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload and current one is UAT.
+          I was changed on the feature branch.
         </p>
         <a
           className="App-link"
